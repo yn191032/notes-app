9c92af194d99693403d938c4f312efa363e0ade7
yn191032 <62829368+yn191032@users.noreply.github.com>
2020-09-24 11:27:07 +0300

    Updates

---
+++ precache-manifest.ea24caafd92f7186ae2ac355a6870f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140f6e5929c5cb6be274e9a0085c02d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75007d00b8c5420e649e",
+    "url": "/static/css/main.843892a3.chunk.css"
+  },
+  {
+    "revision": "357a33dd08348cb292f8",
+    "url": "/static/js/2.f4b40cd2.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/static/js/2.f4b40cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75007d00b8c5420e649e",
+    "url": "/static/js/main.aac13add.chunk.js"
+  },
+  {
+    "revision": "822741c3dcc2c5eee2c5",
+    "url": "/static/js/runtime-main.a17ee0f4.js"
+  }
+]);
