2c4a39163ca8546a13cd1c567e7071cad2d937de
yn191032 <62829368+yn191032@users.noreply.github.com>
2020-09-24 11:31:29 +0300

    Updates

---
+++ precache-manifest.f9e71027cde957b91fa78be7da1f1f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52f580b13f7d14363eb99edf11ce350",
+    "url": "https://yn191032.github.io/notes-app/index.html"
+  },
+  {
+    "revision": "522ca81e2ea2f56a11b2",
+    "url": "https://yn191032.github.io/notes-app/static/css/main.843892a3.chunk.css"
+  },
+  {
+    "revision": "357a33dd08348cb292f8",
+    "url": "https://yn191032.github.io/notes-app/static/js/2.f4b40cd2.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "https://yn191032.github.io/notes-app/static/js/2.f4b40cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522ca81e2ea2f56a11b2",
+    "url": "https://yn191032.github.io/notes-app/static/js/main.04d3581f.chunk.js"
+  },
+  {
+    "revision": "3d380bd6142661b00a84",
+    "url": "https://yn191032.github.io/notes-app/static/js/runtime-main.3f537399.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea24caafd92f7186ae2ac355a6870f69.js"
+  "https://yn191032.github.io/notes-app/precache-manifest.f9e71027cde957b91fa78be7da1f1f46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://yn191032.github.io/notes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
